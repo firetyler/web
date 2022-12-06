f46bd79ea21dd27549c8649f18e50ffcedd1683d
firetyler <oliver-z-jonsson@hotmail.com>
2022-12-06 21:32:41 +0100

    button version 2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
@@ -30,7 +30,8 @@ import { HeaderComponent } from './header/header.component';
     MatListModule,
     MatButtonToggleModule,
     MatRadioModule,
-    MatMenuModule
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
