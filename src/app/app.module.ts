6a066101eaeb877771c20f0b6a8c7032548b366c
firetyler <oliver-z-jonsson@hotmail.com>
2022-12-06 20:43:44 +0100

    button version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,12 @@ import {MatRadioModule} from "@angular/material/radio";
 import {MatSelectModule} from "@angular/material/select";
 import {MatTableModule} from "@angular/material/table";
 import {MatListModule} from "@angular/material/list";
+
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
