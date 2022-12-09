be8f3b427c61408dbb12861eb68cf1e4e2ec6ebe
18mosa02 <mohammadsafieh@live.com>
2022-12-09 10:05:25 +0100

    MiniHeader skapat med RadioButtons issue 8.1,7.1

---
+++ src/app/mini-header/mini-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mini-header',
+  templateUrl: './mini-header.component.html',
+  styleUrls: ['./mini-header.component.css']
+})
+export class MiniHeaderComponent {
+  constructor() { }
+  ngOnInit(): void {
+  }
+}
