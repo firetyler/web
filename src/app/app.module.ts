e75649f98156f44fa8caf527f12499819b4b1bcd
18mosa02 <mohammadsafieh@live.com>
2022-12-09 10:02:23 +0100

    MiniHeader skapat med RadioButtons issue 8.1,7.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MiniHeaderComponent } from './mini-header/mini-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiniHeaderComponent
   ],
   imports: [
     BrowserModule
