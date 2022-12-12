f754aa092540346d3fcdf47d460f54a4ccadf486
18mosa02 <mohammadsafieh@live.com>
2022-12-12 11:42:37 +0100

    Time Container Right side of the Website

--- src/app/time-filters/time-filters.component.ts
+++ src/app/time-filters/time-filters.component.ts
@@ -5,7 +5,9 @@ interface Year {
 }
 let  date = new Date();
 let year = date.getFullYear();
-let day = date.getDay();
+let dayName = date.getDay(); // 0 to 6 it returns the day of the week
+
+
 
 @Component({
   selector: 'app-time-filters',
@@ -17,7 +19,22 @@ export class TimeFiltersComponent {
 constructor() {
 
 }
+  ngOnInit(): void {
+    this.getWeekNumber();
+    console.log(this.getWeekNumber());
+  }
 
+ getWeekNumber() {
+    let date = new Date();
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    let week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+      - 3 + (week1.getDay() + 6) % 7) / 7);
+  }
 
 
   // @ts-ignore
