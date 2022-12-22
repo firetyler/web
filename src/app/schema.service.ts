d67ba112beeb7bfc57e84ccc76e92353372cdbc7
jenniferstreit <jenniferstreit@hotmail.com>
2022-12-22 11:25:54 +0100

    funkar fortfarande inte

--- src/app/schema.service.ts
+++ src/app/schema.service.ts
@@ -18,15 +18,10 @@ export class SchemaService {
         'Content-Type': 'text/xml'
       })
     }
+
     const sr = '<?xml version=\"1.0\"?> <soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tjan=\"http://www.kronox.se/webb/tjanster/\" xmlns:typ=\"http://www.kronox.se/webb/tjanster/typer\"> <soapenv:Header/><soapenv:Body> <tjan:hamtaSchemaPosterOchForklaringsTexterForVillkorMedIntervall> <arg0> <typ:eventuelltKlientId>?</typ:eventuelltKlientId> <typ:intervallAntal>5</typ:intervallAntal><typ:intervallTyp>d</typ:intervallTyp><typ:startDatum>2021-12-02</typ:startDatum><typ:villkor><typ:aktivitetstyp>?</typ:aktivitetstyp><typ:bokningstyp>?</typ:bokningstyp><typ:extraAttribut></typ:extraAttribut><typ:filterTyp>resurser</typ:filterTyp><typ:filterVarden>l.99518</typ:filterVarden><typ:moment>?</typ:moment><typ:orgenheterRESURSER>?</typ:orgenheterRESURSER><typ:orgenheterUTB>?</typ:orgenheterUTB><typ:sokMedAND>?</typ:sokMedAND><typ:sprak>?</typ:sprak><typ:taMedForklaringar>false</typ:taMedForklaringar></typ:villkor></arg0></tjan:hamtaSchemaPosterOchForklaringsTexterForVillkorMedIntervall></soapenv:Body></soapenv:Envelope>';
 
-    const svar = this.http.post(url, sr, {headers: new HttpHeaders()
-        .set('Content-Type', 'text/xml')
-        .append('Access-Control-Allow-Methods','POST')
-        .append('Access-Control-Allow-Origin', '*')
-        .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method"),
-      responseType: 'text'
-    }).subscribe();
+    const svar = this.http.post(url, sr, httpOptions).subscribe();
     console.log(svar);
     return svar;
 
