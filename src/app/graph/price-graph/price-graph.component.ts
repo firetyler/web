33d621a9d93b14576d85392aacec9ccf18f0df95
firetyler <oliver-z-jonsson@hotmail.com>
2023-01-03 10:37:45 +0100

    reformatting of graph and making it JSON array compatible

--- src/app/graph/price-graph/price-graph.component.ts
+++ src/app/graph/price-graph/price-graph.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {SeviceService} from "../sevice.service";
 import {Month} from "../month";
 import {Days} from "../days";
+import {TimeFiltersComponent} from "../../time-filters/time-filters.component";
 
 declare var google: any;
 
@@ -11,7 +12,7 @@ declare var google: any;
   styleUrls: ['./price-graph.component.css']
 })
 export class PriceGraphComponent implements OnInit {
-
+  @Input() value : any;
   constructor(private service : SeviceService) {
   }
 
@@ -19,16 +20,22 @@ export class PriceGraphComponent implements OnInit {
     google.charts.load('current', {'packages': ['corechart']});
     google.charts.setOnLoadCallback(this.drawChart);
   }
-  drawChart() {
-    const data = google.visualization.arrayToDataTable([
-      ['ID', 'date', 'bokandeTimmar', 'akademi', 'totaltid'],
-      ['29111', 12, 2, 'ATM', 33739900],
 
+  drawChart(json : any) {
+    const jsonn = [
+      {'ID' : "99123" , 'Date' : new Date(2020,12,1), 'bookedTime' : 2, 'akademi' : "atm"}];
+    const  carray :any[] = [['ID', 'date','bookedTime','akademi']];
 
-    ]);
+    for (let i = 0;i < jsonn.length;i++){
+      carray.push([jsonn[i].ID, jsonn[i].Date,jsonn[i].bookedTime, jsonn[i].akademi]);
+    }
+
+    const data = google.visualization.arrayToDataTable(carray);
     const options = {
       title: '',
-      hAxis: {title: 'dagar'},
+      hAxis: {title: 'dagar',
+        format : ' dd mm yyyy'
+      },
       vAxis: {title: 'timmar'},
       bubble: {
         textStyle: {fontSize: 13},
--- src/app/graph/sevice.service.ts
+++ src/app/graph/sevice.service.ts
@@ -1,6 +1,7 @@
-import {Injectable, OnChanges, OnInit} from '@angular/core';
+import {Injectable, Input, OnChanges, OnInit} from '@angular/core';
 import {Days} from "./days";
 import {Month} from "./month";
+import {TimeFiltersComponent} from "../time-filters/time-filters.component";
 
 
 @Injectable({
@@ -9,28 +10,21 @@ import {Month} from "./month";
 
 
 export class SeviceService implements OnChanges {
+  private _year: any;
+
+  private _month: any;
+  private _hour: any;
+  private _minutes: any;
+  private _totalTime: any;
 
 
   constructor() {
   }
 
   ngOnChanges(): void {
-
   }
-
-  private _year: any;
-  private _month: any;
-  private _hour: any;
-  private _minutes: any;
-  private _startTime: any;
-  private _endTime: any;
-  private _totalTime: any;
-
-
 // 6072 på ett år
   totalTimOFYear: number = 6072;
-
-
   getAllMonth() {
    const m = Object.keys(Month).filter((v) => isNaN(Number(v)));
    m.forEach((value) =>{
@@ -43,18 +37,4 @@ export class SeviceService implements OnChanges {
       return value;
     });
   }
-  setTImeBorder(value : number , overTime :boolean){
-   let max =8;
-   let min =0;
-   if (min <= value  && value <= max && overTime == false){
-     return value;
-   }else{
-     return value;
-   }
-  }
- SpecifiedDaysInMonth(month : Month , year : number ){
-    return new Date(year , month,0).getDate();
-
- }
-
 }
