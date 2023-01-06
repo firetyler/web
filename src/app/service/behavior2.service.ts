08c5e5cbe6bc2ade11b1b44b136901447d14c36c
Ann Johansson <ann.j@live.se>
2023-01-06 13:41:52 +0100

    fixed behavior2.service.ts

--- src/app/service/behavior2.service.ts
+++ src/app/service/behavior2.service.ts
@@ -1,56 +1,81 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ScheduleEntry} from "./schema.service";
 import {CsvFileReaderService} from "./csv-file-reader.service";
+import {startWith} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class Behavior2Service {
-  private testScheduleEntry:any[] = [new ScheduleEntry("2012-01-01 08:00:00",
-    "2012-01-01 10:00:00","11121","DVG002")];
-  private roomBehavior : Behavior[] = [];
-  constructor(private schedule : ScheduleEntry, private room : CsvFileReaderService) {
+  private testScheduleEntry: any[] = [new ScheduleEntry("2012-01-01 08:00:00",
+    "2012-01-01 10:00:00", "11121", "DVG002")];
+  private roomBehavior: Behavior[] = [];
+
+  constructor(private schedule: ScheduleEntry, private room: CsvFileReaderService) {
     const allRooms = room.getRooms();
-    for(let i = 0 ; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
       if (this.testScheduleEntry[0].room === allRooms[i].id) {
         this.roomBehavior.push(new Behavior(allRooms[i].id, this.testScheduleEntry));
       }
     }
   }
 }
+
 export class Behavior {
-  room : number;
-  color : string = "";
-  date : any;
-  bookings : ScheduleEntry[];
-  private dateError : boolean = false;
+  room: number;
+  color: string = "";
+  date: any;
+  bookings: ScheduleEntry[];
+  private dateError: boolean = false;
   private HOURS_OF_A_WORK_DAY = 8;
 
-  constructor(room : number, bookings : ScheduleEntry[]) {
+  constructor(room: number, bookings: ScheduleEntry[]) {
     this.room = room;
     this.setColor();
     this.setDate();
     this.bookings = bookings;
   }
 
-  setColor(){
+  setColor() {
     let hasBadBehavior = false;
     let totalTime = 0;
+    let beforeLunch;
+    let afterLunch;
     for (let i = 0; i < this.bookings.length; i++) {
-      totalTime += this.bookings[i].getTotalHours();
-    }
-    for (let i = 0; i < this.bookings.length; i++) {
-      if ((this.getMilitaryTime(this.bookings[i].startTime) < 1200 &&
-        this.getMilitaryTime(this.bookings[i].startTime) > 930)){
+      let startTime = this.getMilitaryTime(this.bookings[i].startTime);
+      let endTime = this.getMilitaryTime(this.bookings[i].endTime);
 
+      if (endTime > 1145 && endTime < 1215) {
+        beforeLunch = this.bookings[i];
+      } else if (startTime > 1245 && startTime < 1330) {
+        afterLunch = this.bookings[i];
       }
     }
 
-    this.color = "#ff0000"
+    if (beforeLunch != undefined && afterLunch != undefined) {
+      if (beforeLunch.course === afterLunch.course) {
+        if (beforeLunch.getTotalHours() + afterLunch.getTotalHours() <= 4) {
+          hasBadBehavior = true;
+        }
+      }
+    }
+// TODO Byt ut loop mot olivers service
+    for (let i = 0; i < this.bookings.length; i++) {
+      totalTime += this.bookings[i].getTotalHours();
+    }
+    if (totalTime > 7 && !hasBadBehavior) {
+      this.color = '#0000ff';
+    } else if (this.bookings.length == 2 && hasBadBehavior) {
+      this.color = "#ff0000";
+    } else if (this.bookings.length > 2 && hasBadBehavior) {
+      this.color = '#ff8c00';
+    } else {
+      this.color = '#ffff00';
+    }
   }
 
   getMilitaryTime(time: string): number {
-    const militaryTime = time.replace(':','');
+    const militaryTime = time.replace(':', '');
     return parseInt(militaryTime);
   }
 
@@ -59,10 +84,16 @@ export class Behavior {
     for (let i = 0; i < this.bookings.length; i++) {
       if (this.bookings[i].startDate !== this.bookings[i].endDate) {
         this.dateError = true;
-      } else if (this.date !== this.bookings[i].startDate){
+      } else if (this.date !== this.bookings[i].startDate) {
         this.dateError = true;
       }
     }
-    this.date = new Date(0,0,0);
+    if(this.dateError){
+      this.date = new Date(0, 0, 0);
+    }else {
+      let split = this.bookings[0].startDate.split("-");
+      this.date = new Date(parseInt(split[0]), parseInt(split[1]), parseInt(split[2]));
+    }
+
   }
 }
