21812096bc47d3c284012a03551759cc7a03fca8
Adam Chabchoub <adamchabchoub87@gmail.com>
2023-02-20 14:53:15 +0100

    Lite design

--- src/app/mini-header/mini-header.component.ts
+++ src/app/mini-header/mini-header.component.ts
@@ -2,6 +2,7 @@ import {Component, Injectable, OnChanges, OnInit, SimpleChanges} from '@angular/
 import {MAT_RADIO_DEFAULT_OPTIONS} from "@angular/material/radio";
 import {AppComponent} from "../app.component";
 import {MainComponent} from "../main/main.component";
+import {SearchBarComponent} from "../filter-bar/component/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-mini-header',
@@ -27,5 +28,8 @@ export class MiniHeaderComponent {
   async getGraph(){
     return this.main.getKey();
   }
+  /*async submitFunction(){
+    await this.filter.submitFunction();
+  }*/
 }
 
